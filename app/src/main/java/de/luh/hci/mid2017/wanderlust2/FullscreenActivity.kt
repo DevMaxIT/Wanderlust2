8668d8ec4fde3b28a0a4f8dabaefd74175e8d989
Maximilian Friedrich <maximilian.friedrich@paraloom.de>
2017-12-07 12:14:17 +0100

    no message

--- app/src/main/java/de/luh/hci/mid2017/wanderlust2/FullscreenActivity.kt
+++ app/src/main/java/de/luh/hci/mid2017/wanderlust2/FullscreenActivity.kt
@@ -4,13 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import kotlinx.android.synthetic.main.activity_intro.*
+import kotlinx.android.synthetic.main.activity_fullscreen.*
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class Intro : AppCompatActivity() {
+class FullscreenActivity : AppCompatActivity() {
     private val mHideHandler = Handler()
     private val mHidePart2Runnable = Runnable {
         // Delayed removal of status and navigation bar
@@ -48,7 +48,7 @@ class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_fullscreen)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         mVisible = true
