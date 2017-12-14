2917c19c193c5ed42f96ebaf5e7d1b7f569e6f17
TimTimSOS <34125708+TimTimSOS@users.noreply.github.com>
2017-12-14 11:38:52 +0100

    Mapview2

---
+++ app/src/main/java/de/luh/hci/mid2017/wanderlust2/MapActivity.kt
@@ -0,0 +1,12 @@
+package de.luh.hci.mid2017.wanderlust2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
