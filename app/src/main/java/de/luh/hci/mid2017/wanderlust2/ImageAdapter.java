826e70e73389c575ccd0a29aa5115112decdf4c1
Maximilian Friedrich <maximilian.friedrich@paraloom.de>
2017-12-21 11:57:58 +0100

    gridview added

---
+++ app/src/main/java/de/luh/hci/mid2017/wanderlust2/ImageAdapter.java
@@ -0,0 +1,86 @@
+package de.luh.hci.mid2017.wanderlust2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by jobusch on 13.12.17.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private LayoutInflater mInflater;
+
+
+    public ImageAdapter(Context c) {
+        mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.griditem, null);
+
+            holder.imageview = (ImageView) convertView.findViewById(R.id.imageView);
+            holder.checkbox = (CheckBox) convertView.findViewById(R.id.checkBox);
+
+            convertView.setTag(holder);
+
+            //holder.imageview.setLayoutParams(new GridView.LayoutParams(285, 285));
+            holder.imageview.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            holder.imageview.setPadding(8, 8, 8, 8);
+
+            //imageView = new ImageView(mContext);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.checkbox.setId(position);
+        holder.imageview.setId(position);
+        holder.id = position;
+        holder.imageview.setImageResource(mThumbIds[position]);
+       return convertView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+    class ViewHolder {
+        ImageView imageview;
+        CheckBox checkbox;
+        int id;
+    }
+}
--- app/src/main/java/de/luh/hci/mid2017/wanderlust2/SelectPathActivity.kt
+++ app/src/main/java/de/luh/hci/mid2017/wanderlust2/SelectPathActivity.kt
@@ -2,16 +2,25 @@ package de.luh.hci.mid2017.wanderlust2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_select_path.*
+import android.view.View
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
 
 class SelectPathActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_path)
-        setSupportActionBar(toolbar)
+        //setSupportActionBar(toolbar)
 
+        val gridview = findViewById<View>(R.id.gridview) as GridView
+        gridview.adapter = ImageAdapter(this)
 
+        gridview.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+            Toast.makeText(this@SelectPathActivity, "" + position,
+                    Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
