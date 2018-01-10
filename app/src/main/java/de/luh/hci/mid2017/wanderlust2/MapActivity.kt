f2f7ba0b126cda0d2cc190cb35059ffd07dd732e
TimTimSOS <34125708+TimTimSOS@users.noreply.github.com>
2018-01-10 17:20:30 +0100

    Map geht noch nicht, kleine änderung

--- app/src/main/java/de/luh/hci/mid2017/wanderlust2/MapActivity.kt
+++ app/src/main/java/de/luh/hci/mid2017/wanderlust2/MapActivity.kt
@@ -4,8 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +22,18 @@ class MapActivity : AppCompatActivity() {
         buttonEditPath.setOnClickListener({
             val intent = Intent(this@MapActivity, SelectPathActivity::class.java)
             startActivity(intent)
+            val mapFragment = supportFragmentManager
+                    .findFragmentById(R.id.fragment) as SupportMapFragment
+            mapFragment.getMapAsync(this)
 
         })
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
