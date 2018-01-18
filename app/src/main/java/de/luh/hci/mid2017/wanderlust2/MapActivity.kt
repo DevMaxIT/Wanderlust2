19757096a97c350ccd790ae14a33b87ed291e6b0
Johannes Busch <jobusch95(at)gmail.com>
2018-01-18 12:35:58 +0100

    map eingebaut

--- app/src/main/java/de/luh/hci/mid2017/wanderlust2/MapActivity.kt
+++ app/src/main/java/de/luh/hci/mid2017/wanderlust2/MapActivity.kt
@@ -2,16 +2,24 @@ package de.luh.hci.mid2017.wanderlust2
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import android.widget.Toast
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+import android.text.method.TextKeyListener.clear
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.GoogleMap
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+
+
+class MapActivity : FragmentActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
@@ -22,18 +30,26 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         buttonEditPath.setOnClickListener({
             val intent = Intent(this@MapActivity, SelectPathActivity::class.java)
             startActivity(intent)
-            val mapFragment = supportFragmentManager
-                    .findFragmentById(R.id.fragment) as SupportMapFragment
-            mapFragment.getMapAsync(this)
-
         })
+        val buttonstartRoute = findViewById<Button>(R.id.button2)
+        buttonstartRoute.setOnClickListener({
+            Toast.makeText(this, "Navigation wird auf der Uhr gestartet.",
+                    Toast.LENGTH_SHORT).show()
+        })
+        val mapFragment = fragmentManager
+                .findFragmentById(R.id.map) as MapFragment
+        mapFragment.getMapAsync(this)
     }
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //val sydney = LatLng(-34.0, 151.0)
+        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
     }
+
+
 }
